c3c9e78901a91c192fd169a94210609e36132a22
BlueNakoTech <nicholausd46@gmail.com>
2023-10-27 20:07:35 +0700

    jsn

--- interaction/buttonInteraction.js
+++ interaction/buttonInteraction.js
@@ -29,8 +29,8 @@ module.exports = async (interaction) => {
     // Format the data and add it to the embed fields
     data.map((document) => {
       const name = document.Nama.toString();
-      const regId = document.id // Replace 'name' with your actual field name
-      embedFields.push({ name: name, value: `\`Reg ID : ${regId}\``, inline:false });
+      const regId = document.Discord.toString(); // Replace 'name' with your actual field name
+      embedFields.push({ name: name, value: `\`Discord User : ${regId}\``, inline:false });
     });
     const options = data.map((doc) => {
       const option = new StringSelectMenuOptionBuilder()
